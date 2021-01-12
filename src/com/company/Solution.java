f2d0ab06e51a127d90b49fcb20789605e4871259
Nikolay <n.filatov95@ya.ru>
2021-01-12 12:50:36 +0400

    Initial commit

---
+++ src/com/company/Solution.java
@@ -0,0 +1,42 @@
+package com.company;
+import java.util.Scanner;
+
+/*
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Введите число:");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int event = 0;
+        int odd = 0;
+
+        int number_length = stringSize(number);
+
+            for (int i = 0; i < number_length; i++) {
+                    if ( (number % 10) % 2 == 0 )
+                        event++;
+                    else
+                        odd++;
+                    number /= 10;
+            }
+        System.out.println("Even: " + event + " Odd: " + odd);
+            scanner.close();
+
+    }
+
+    final static int [] sizeTable = { 9, 99, 999, 9999, 99999, 999999, 9999999,
+            99999999, 999999999, Integer.MAX_VALUE };
+
+    // Requires positive x
+    static int stringSize(int x) {
+        for (int i=0; ; i++)
+            if (x <= sizeTable[i])
+                return i+1;
+    }
+}
